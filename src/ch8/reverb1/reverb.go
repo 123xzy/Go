f077828135184547c6287c534f404b365e2b23c4
123xzy <1037884309@qq.com>
2019-05-13 00:13:12 -0700

    update ch8

---
+++ src/ch8/reverb1/reverb.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+	"fmt"
+	"bufio"
+	"net"
+	"time"
+	"strings"
+	"log"
+)
+
+func echo(c net.Conn,shout string,delay time.Duration){
+	fmt.Fprintln(c,"\t",strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c,"\t",shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c,"\t",strings.ToLower(shout))
+}
+
+func handlerConn(c net.Conn){
+	input := bufio.NewScanner(c)
+	for input.Scan(){
+		echo(c,input.Text(),time.Second*2)
+	}
+	c.Close()
+}
+
+func main(){
+	l,err := net.Listen("tcp","localhost:8000")
+	if err != nil{
+		log.Fatal(err)
+	}
+	for{
+		conn,err := l.Accept()
+		if err != nil{
+			log.Print(err)
+			continue
+		}
+		go handlerConn(conn)
+	}
+}
